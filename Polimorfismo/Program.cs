265c13fb9497a284d3f8005061ea9359f5699fd3
Dayane <dayanetm@outlook.com>
2024-07-22 15:30:02 -0300

    treinando polimorfismo em poo

---
+++ Polimorfismo/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+Crie uma classe FormaGeometrica com métodos como CalcularArea.
+Implemente as subclasses Retangulo, Circulo e Triangulo, fornecendo cálculos específicos para cada forma.
+*/
+
+using System;
+
+namespace AcademiaDotNet;
+
+public class Program {
+
+    public static void Main(string[] args) {
+        
+    }
+}
